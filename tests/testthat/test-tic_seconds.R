a1862f7fd6ce0263c932c991c060ef3e77954fc7
Daniel Anderson <daniela@uoregon.edu>
2020-12-04 11:47:42 -0800

    fix tests 🤞

--- tests/testthat/test-tic_seconds.R
+++ tests/testthat/test-tic_seconds.R
@@ -7,8 +7,8 @@ tic()
 m <- lm(mpg ~ hp, mtcars)
 toc(log = TRUE)
 
-time_check <- as.character(tic_seconds(1))
-digit_check1 <- as.character(tic_seconds(1))
+time_check <- as.character(tic_seconds(1, 0))
+digit_check1 <- as.character(tic_seconds(1, 0))
 digit_check2 <- as.character(tic_seconds(1, 3))
 
 test_that("tic_seconds works", {
